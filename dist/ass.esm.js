7fee9a0ae942f77b7e3237dd04d34269eafbc283
Miguel Correia <miguelncorreia@gmail.com>
2020-01-08 17:35:43 +0100

    Word break fixes

--- dist/ass.esm.js
+++ dist/ass.esm.js
@@ -1460,19 +1460,19 @@ function createDrawing(fragment, styleTag) {
 
 function encodeText(text, q) {
   return text
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/\s/g, '&nbsp;')
-    .replace(/\\h/g, '&nbsp;')
-    .replace(/\\N/g, '<br>')
-    .replace(/\\n/g, q === 2 ? '<br>' : '&nbsp;');
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/\s/g, "&nbsp;")
+    .replace(/\\h/g, "&nbsp;")
+    .replace(/\\N/g, "<br>")
+    .replace(/\\n/g, q === 2 ? "<br>" : "&nbsp;");
 }
 
 function createDialogue(dialogue) {
   var this$1 = this;
 
-  var $div = document.createElement('div');
-  $div.className = 'ASS-dialogue';
+  var $div = document.createElement("div");
+  $div.className = "ASS-dialogue";
   var df = document.createDocumentFragment();
   var slices = dialogue.slices;
   var start = dialogue.start;
@@ -1484,73 +1484,86 @@ function createDialogue(dialogue) {
       var drawing = fragment.drawing;
       var animationName = fragment.animationName;
       var tag = assign({}, slice.tag, fragment.tag);
-      var cssText = 'display:inline-block;';
+      var cssText = "display:inline-block;";
       var vct = this$1.video.currentTime;
       if (!drawing) {
         cssText += "font-family:\"" + (tag.fn) + "\",Arial;";
-        cssText += "font-size:" + (this$1.scale * getRealFontSize(tag.fn, tag.fs)) + "px;";
+        cssText += "font-size:" + (this$1.scale *
+          getRealFontSize(tag.fn, tag.fs)) + "px;";
         cssText += "color:" + (color2rgba(tag.a1 + tag.c1)) + ";";
-        var scale = /Yes/i.test(this$1.info.ScaledBorderAndShadow) ? this$1.scale : 1;
+        var scale = /Yes/i.test(this$1.info.ScaledBorderAndShadow)
+          ? this$1.scale
+          : 1;
         if (borderStyle === 1) {
           cssText += "text-shadow:" + (createCSSStroke(tag, scale)) + ";";
         }
         if (borderStyle === 3) {
-          cssText += (
-            "background-color:" + (color2rgba(tag.a3 + tag.c3)) + ";"
-            + "box-shadow:" + (createCSSStroke(tag, scale)) + ";"
-          );
+          cssText +=
+            "background-color:" + (color2rgba(tag.a3 + tag.c3)) + ";" +
+            "box-shadow:" + (createCSSStroke(tag, scale)) + ";";
         }
-        cssText += tag.b ? ("font-weight:" + (tag.b === 1 ? 'bold' : tag.b) + ";") : '';
-        cssText += tag.i ? 'font-style:italic;' : '';
-        cssText += (tag.u || tag.s) ? ("text-decoration:" + (tag.u ? 'underline' : '') + " " + (tag.s ? 'line-through' : '') + ";") : '';
-        cssText += tag.fsp ? ("letter-spacing:" + (tag.fsp) + "px;") : '';
-        // TODO: (tag.q === 0) and (tag.q === 3) are not implemented yet,
-        // for now just handle it as (tag.q === 1)
-        if (tag.q === 1 || tag.q === 0 || tag.q === 3) {
-          cssText += 'word-break:break-all;white-space:normal;';
-        }
-        if (tag.q === 2) {
-          cssText += 'word-break:normal;white-space:nowrap;';
+        cssText += tag.b ? ("font-weight:" + (tag.b === 1 ? "bold" : tag.b) + ";") : "";
+        cssText += tag.i ? "font-style:italic;" : "";
+        cssText +=
+          tag.u || tag.s
+            ? ("text-decoration:" + (tag.u ? "underline" : "") + " " + (tag.s ? "line-through" : "") + ";")
+            : "";
+        cssText += tag.fsp ? ("letter-spacing:" + (tag.fsp) + "px;") : "";
+
+        if (tag.q === 0 || tag.q === 3) {
+          cssText += "word-break:break-word;white-space:normal;";
+        } else if (tag.q === 1) {
+          cssText += "word-break:break-all;white-space:normal;";
+        } else if (tag.q === 2) {
+          cssText += "word-break:normal;white-space:nowrap;";
         }
       }
-      var hasTransfrom = transformTags.some(function (x) { return (
-        /^fsc[xy]$/.test(x) ? tag[x] !== 100 : !!tag[x]
-      ); });
+      var hasTransfrom = transformTags.some(function (x) { return /^fsc[xy]$/.test(x) ? tag[x] !== 100 : !!tag[x]; }
+      );
       if (hasTransfrom) {
         cssText += (vendor.transform) + "transform:" + (createTransform(tag)) + ";";
         if (!drawing) {
-          cssText += 'transform-style:preserve-3d;word-break:normal;white-space:nowrap;';
+          cssText +=
+            "transform-style:preserve-3d;word-break:normal;white-space:nowrap;";
         }
       }
       if (animationName) {
-        cssText += createAnimation(animationName, end - start, Math.min(0, start - vct));
+        cssText += createAnimation(
+          animationName,
+          end - start,
+          Math.min(0, start - vct)
+        );
       }
       if (drawing && tag.pbo) {
-        var pbo = this$1.scale * -tag.pbo * (tag.fscy || 100) / 100;
+        var pbo = (this$1.scale * -tag.pbo * (tag.fscy || 100)) / 100;
         cssText += "vertical-align:" + pbo + "px;";
       }
 
+      var replaceSpaces = new RegExp("&nbsp;", "g");
       var hasRotate = /"fr[xyz]":[^0]/.test(JSON.stringify(tag));
-      encodeText(text, tag.q).split('<br>').forEach(function (html, idx) {
-        var $span = document.createElement('span');
-        $span.dataset.hasRotate = hasRotate;
-        if (drawing) {
-          var obj = createDrawing.call(this$1, fragment, slice.tag);
-          $span.style.cssText = obj.cssText;
-          $span.appendChild(obj.$svg);
-        } else {
-          if (idx) {
-            df.appendChild(document.createElement('br'));
+
+      encodeText(text, tag.q)
+        .split("<br>")
+        .forEach(function (html, idx) {
+          var $span = document.createElement("span");
+          $span.dataset.hasRotate = hasRotate;
+          if (drawing) {
+            var obj = createDrawing.call(this$1, fragment, slice.tag);
+            $span.style.cssText = obj.cssText;
+            $span.appendChild(obj.$svg);
+          } else {
+            if (idx) {
+              df.appendChild(document.createElement("br"));
+            }
+            if (!html) {
+              return;
+            }
+            $span.innerHTML = html.replace(replaceSpaces, " ");
           }
-          if (!html) {
-            return;
-          }
-          $span.innerHTML = html;
-        }
-        // TODO: maybe it can be optimized
-        $span.style.cssText += cssText;
-        df.appendChild($span);
-      });
+          // TODO: maybe it can be optimized
+          $span.style.cssText += cssText;
+          df.appendChild($span);
+        });
     });
   });
   $div.appendChild(df);
